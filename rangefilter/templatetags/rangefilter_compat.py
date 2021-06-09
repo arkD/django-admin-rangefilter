8337a43d71f0d37672a586f60d14808c4d331734
ArkD <aaronmdewitt@gmail.com>
2021-06-09 15:36:43 -0500

    Update rangefilter_compat.py
    admin_static fing grr

--- rangefilter/templatetags/rangefilter_compat.py
+++ rangefilter/templatetags/rangefilter_compat.py
@@ -6,10 +6,7 @@ import django
 
 from django.template import Library
 
-if django.VERSION[:2] >= (1, 10):
-    from django.templatetags.static import static as _static
-else:
-    from django.contrib.admin.templatetags.admin_static import static as _static
+from django.templatetags.static import static as _static
 
 register = Library()
 
